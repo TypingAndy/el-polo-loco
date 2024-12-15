494743b5b520e9c82b2aee6ff804977e5deacd9a
TypingAndy <andreas.georg@outlook.com>
2024-12-15 17:03:57 +0100

    2,13, level Object

--- models/background-objects.class.js
+++ models/background-objects.class.js
@@ -3,9 +3,9 @@ class BackgroundObjects extends MovableObject{
   width = 720;
   height = 480;
 
-  constructor(imagePath) {
+  constructor(imagePath, x) {
     super().loadImage(imagePath);
     this.y = 0;
-    this.x = 0;
+    this.x = x;
   }
 }
